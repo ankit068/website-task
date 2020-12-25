5c948d47040ea69143b427a41be315f009d51273
ANKIT <32790414+ankit068@users.noreply.github.com>
2020-12-25 13:33:10 +0530

    new commit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // NAV
-gsap.from('h1', {opacity: 0, duration: 1, delay:1.1, y: 10})
-gsap.from('.heading,.social-icons', {opacity: 0, duration: 1, delay: 1.4, y: 30, stagger: 0.2,})
+gsap.from('h1', {scale:2,opacity: 0, duration: 1, delay:1.1, x: 10,})
+gsap.from('.heading,.social-icons', {opacity: 0, duration: 1, delay: 1.4, x: 30, stagger: 0.2,})
 
 // HOME
-gsap.from('.container1,.container2,.container3,.container4', {opacity: 0, duration: 1, delay:1.7, y: 30,stagger: 0.2})
-gsap.from('h5,p', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.cam,.numbers', {opacity: 0, duration: 1, delay:1.9, y: 30, stagger: 0.2})
-gsap.from('.icons,.text', {opacity: 0, duration: 1, delay:2, y: 30})
+gsap.from('.container1,.container2,.container3,.container4', {scale:2, opacity: 0, duration: 1, delay:1.7, y: 30,stagger: 0.2})
+gsap.from('h5,p', {opacity: 0, duration: 1, delay:1.8, z: 30,})
+gsap.from('.cam,.numbers', {opacity: 0, duration: 1, delay:1.9, x: 30, stagger: 0.2})
+gsap.from('.icons,.text', {opacity: 0, duration: 1, delay:2, y: 30,})
+
